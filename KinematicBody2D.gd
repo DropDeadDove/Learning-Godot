b64e242c6f46895da5d6827a0931875f3efd1971
DropDeadDove <callum-robinson.spam@hotmail.com>
2019-03-30 01:00:58 +0000

    completed tutorial 4

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,16 +1,41 @@
 extends KinematicBody2D
 
+const UP = Vector2( 0 , -1 )
+const GRAVITY = 20
+const ACCELERATION = 50
+const MAX_SPEED = 200
+const JUMP_HEIGHT = -500
+
 var motion = Vector2()
 
 func _physics_process(delta):
+	motion.y += GRAVITY
+	var friction = false
 	
-	if Input.is_action_pressed("ui-right"):
-		motion.x = 100
-	elif Input.is_action_pressed("ui-left"):
-		motion.x = -100
+	if Input.is_action_pressed("ui_right"):
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
 	else:
-		motion.x = 0
+		$Sprite.play("Idle")
+		friction = true
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.3)
+	else:
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
 	
-	
-	move_and_slide(motion)
+	motion = move_and_slide(motion, UP)
 	pass
